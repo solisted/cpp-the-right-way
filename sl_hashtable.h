753e57be0669d8fbd8c036702fdac357a5115f1d
Ivan Farafontov <isolisted@gmail.com>
2025-08-26 00:12:08 -0500

    Episode 12

---
+++ sl_hashtable.h
@@ -0,0 +1,33 @@
+#ifndef SL_HASHTABLE_H
+#define SL_HASHTABLE_H
+
+#include "sl_string.h"
+#include "sl_arena.h"
+
+#define SL_HASHTABLE_MAX_LOAD 75
+
+typedef size_t sl_hashtable_hash;
+
+typedef struct sl_hashtable_bucket sl_hashtable_bucket;
+typedef struct sl_hashtable sl_hashtable;
+
+struct sl_hashtable_bucket {
+    sl_hashtable_bucket *next;
+    sl_hashtable_hash hash;
+    sl_string *key;
+    sl_string *value;
+};
+
+struct sl_hashtable {
+    sl_hashtable_bucket **buckets;
+    sl_arena *arena;
+    size_t size;
+    size_t count;
+    size_t preallocate;
+};
+
+void sl_hashtable_init(sl_hashtable *hashtable, sl_arena *arena, size_t preallocate);
+sl_string *sl_hashtable_get(sl_hashtable *hashtable, sl_string *key);
+int sl_hashtable_set(sl_hashtable *hashtable, sl_string *key, sl_string *value);
+
+#endif
